c604c650bb957761ccf2b13f07bce2f26d4f2dd6
Robin <choronz@users.noreply.github.com>
2025-07-18 22:13:36 +0800

    Update build.sh

--- .github/workflows/scripts/build.sh
+++ .github/workflows/scripts/build.sh
@@ -12,7 +12,7 @@ LD_LIBRARY_PATH=${cuda_home}/lib64:$LD_LIBRARY_PATH
 $python_executable -m pip install -r requirements/build.txt -r requirements/cuda.txt
 
 # Limit the number of parallel jobs to avoid OOM
-export MAX_JOBS=1
+export MAX_JOBS=2
 # Make sure release wheels are built for the following architectures
 export TORCH_CUDA_ARCH_LIST="8.0 8.9"
 export VLLM_FA_CMAKE_GPU_ARCHES="80-real"
